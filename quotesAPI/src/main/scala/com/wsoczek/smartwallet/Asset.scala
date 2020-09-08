e7d8abf1c1133dee81d95d26d286a5a60592b5df
witek <witus2706@gmail.com>
2020-09-09 00:28:35 +0200

    clean up

--- quotesAPI/src/main/scala/com/wsoczek/smartwallet/Asset.scala
+++ quotesAPI/src/main/scala/com/wsoczek/smartwallet/Asset.scala
@@ -5,16 +5,9 @@ import spray.json.RootJsonFormat
 
 sealed trait Asset
 
-case class Stock(currentPrice: Float,
-                 highestPriceOfDay: Float,
-                 lowestPriceOfDay: Float,
-                 openPrice: Float,
-                 previousClosePrice: Float) extends Asset
+case class Stock(currentPrice: Float, highestPriceOfDay: Float, lowestPriceOfDay: Float, openPrice: Float, previousClosePrice: Float) extends Asset
 
-case class Crypto(openPrice: Float,
-                  highestPriceOfDay: Float,
-                  lowestPriceOfDay: Float,
-                  closePrice: Float) extends Asset
+case class Crypto(openPrice: Float, highestPriceOfDay: Float, lowestPriceOfDay: Float, closePrice: Float) extends Asset
 
 object Asset {
   implicit val stockJsonFormat: RootJsonFormat[Stock] = jsonFormat(Stock, "c", "h", "l", "o", "pc")
